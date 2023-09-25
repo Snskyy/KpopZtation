7fd5ab6dbbe81f30368cbc9479d416eb17b696cd
howar <howar@SANSKY>
2023-09-25 14:22:53 +0700

    Add project files.

---
+++ KpopZtation/AlbumManagement/Controller/UpdateController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KpopZtation.ArtistManagement.Controller
+{
+    public class ArtistController
+    {
+    }
+}
---
+++ KpopZtation/UserAuthentication/Repository/LoginRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KpopZtation.UserAuthentication.Repository
+{
+    public class LoginRepository
+    {
+    }
+}
